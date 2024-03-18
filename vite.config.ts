425993f24db4e4379c906f7804d6c2db3b4bfbef
cpicou <72526564+cpicou-patool@users.noreply.github.com>
2024-03-18 12:29:16 +0100

    Removed useless vuetify

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { configDefaults } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -23,9 +22,6 @@ export default defineConfig({
     environment: 'jsdom',
     exclude: [...configDefaults.exclude, 'e2e/*'],
     root: fileURLToPath(new URL('./', import.meta.url)),
-    deps: {
-      inline: ['vuetify']
-    },
     setupFiles: ['./vitest/setup.ts']
   },
   build: {
